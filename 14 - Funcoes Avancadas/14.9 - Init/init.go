62fd67c68218053fb9999c089aced89478be5fd6
Leonardo Custodio <44417666+leoskiline@users.noreply.github.com>
2025-03-25 12:26:26 -0300

    init

---
+++ 14 - Funcoes Avancadas/14.9 - Init/init.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+var n int = 10
+
+func init() {
+	fmt.Println("Executando a função init")
+	n = 10
+}
+
+func main() {
+	fmt.Println("Função main sendo executada")
+	fmt.Println(n)
+}
