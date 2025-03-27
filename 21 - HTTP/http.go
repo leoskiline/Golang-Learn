4236977fde70b8fc9806ed13554f896634537d3b
Leonardo Custodio <44417666+leoskiline@users.noreply.github.com>
2025-03-26 21:30:07 -0300

    http

--- 21 - HTTP/http.go
+++ 21 - HTTP/http.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	w.Write([]byte(`{"mensagem":"Hello World"}`))
+}
+
+func usuarios(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	w.Write([]byte(`{"nome":"Leonardo"}`))
+}
+
 func main() {
 
+	http.HandleFunc("/home", home)
+
+	http.HandleFunc("/usuarios", usuarios)
+
+	log.Fatal(http.ListenAndServe(":5000", nil))
 }
